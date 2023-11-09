48c032fb80f116b9751b46643b2b5180b533820a
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-11-09 18:52:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
-function alertIt()
-{
-    alert("Hello");
-}
-
 function showEduLevel() {
     var eduListValue = document.getElementById("educationLevel").value;
     var eduSpan = document.getElementById("EduSpan");
-    if(eduListValue == "")
-    {
-        eduSpan.style.display="none";
-    } else 
-    {
-        eduSpan.style.display="inline-block";
-    }
-    
+    eduSpan.style.display = eduListValue === "" ? "none" : "block";
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("applicationForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        var dataIsValid = true;
+
+        var inputs = document.querySelectorAll('input[type=text], input[type=email], input[type=tel], input[type=date], input[type=checkbox], select');
+        inputs.forEach(function(input) {
+            var errorSpan = document.getElementById(input.name + 'Error');
+            if((input.type === "checkbox" && !input.checked) || input.value.trim() === "") {
+                errorSpan.textContent = 'This field is required.';
+                input.classList.add('is-invalid');
+                dataIsValid = false;
+            } else {
+                errorSpan.textContent = '';
+                input.classList.remove('is-invalid');
+            }
+        });
+
+        function validateRadios(radioName) {
+            var radios = document.getElementsByName(radioName);
+            var selected = Array.from(radios).some(radio => radio.checked);
+            var errorSpan = document.getElementById(radioName + 'Error');
+            if (!selected) {
+                errorSpan.textContent = 'Please select an option.';
+                dataIsValid = false;
+            } else {
+                errorSpan.textContent = '';
+            }
+        }
+
+        validateRadios('transport');
+        validateRadios('military');
+
+        if (dataIsValid) {
+            console.log('Data is valid. Form submitted.');
+            this.submit();
+        } else {
+            console.error('Data is not valid. Correct the errors.');
+        }
+    });
+});
