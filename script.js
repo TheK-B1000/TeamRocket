0e347eb43fb4fcd812127f8d33423e0e1fdfa8f1
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-11-11 09:50:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,112 @@
+
 function showEduLevel() {
     var eduListValue = document.getElementById("educationLevel").value;
     var eduSpan = document.getElementById("EduSpan");
-    eduSpan.style.display = eduListValue === "" ? "none" : "block";
+    if(eduListValue == "")
+    {
+        eduSpan.style.display="none";
+    } else 
+    {
+        eduSpan.style.display="inline-block";
+    }
+    
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("applicationForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-        var dataIsValid = true;
 
-        var inputs = document.querySelectorAll('input[type=text], input[type=email], input[type=tel], input[type=date], input[type=checkbox], select');
-        inputs.forEach(function(input) {
-            var errorSpan = document.getElementById(input.name + 'Error');
-            if((input.type === "checkbox" && !input.checked) || input.value.trim() === "") {
-                errorSpan.textContent = 'This field is required.';
-                input.classList.add('is-invalid');
-                dataIsValid = false;
-            } else {
-                errorSpan.textContent = '';
-                input.classList.remove('is-invalid');
-            }
-        });
+function validateForm() {
+    let email = document.getElementById("email");
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let mobile = document.getElementById("phone");
+    let homePhone = document.getElementById("homePhone");
+    let startDate = document.getElementById("start-date");
+    let maxHours = document.getElementById("max-hours");
+    let militaryService = document.getElementById("militaryService");
+    let ssn = document.getElementById("ssn");
+    validatEmail(email);
+    validateName(firstName);
+    validateName(lastName);
+    validatePhoneNumber(mobile);
+    validatePhoneNumber(homePhone);
+    validateStartDate(startDate);
+    validateWorkingHours(maxHours);
+    validateMilitaryService(militaryService);
 
-        function validateRadios(radioName) {
-            var radios = document.getElementsByName(radioName);
-            var selected = Array.from(radios).some(radio => radio.checked);
-            var errorSpan = document.getElementById(radioName + 'Error');
-            if (!selected) {
-                errorSpan.textContent = 'Please select an option.';
-                dataIsValid = false;
-            } else {
-                errorSpan.textContent = '';
-            }
-        }
+}
 
-        validateRadios('transport');
-        validateRadios('military');
 
-        if (dataIsValid) {
-            console.log('Data is valid. Form submitted.');
-            this.submit();
-        } else {
-            console.error('Data is not valid. Correct the errors.');
-        }
-    });
-});
+function validatEmail(ele){
+    let regex = /@{1}[a-z]+\.com$/i;
+    if (!ele.value.match(regex))
+    {
+        ele.style.border="2px solid red";
+        return false
+    } else {
+        ele.style.border="1px solid black";
+        return true;
+    }
+}
+
+function validateName(ele)
+{
+    let regex = /[a-zA-Z]/;
+    if(!ele.value.match(regex))
+    {
+        ele.style.border="2px solid red";
+        return false;
+    } else {
+        ele.style.border="1px solid black";
+        return true;
+    }
+}
+
+function validatePhoneNumber(ele)
+{
+    let regex = /[0-9]{3}-?[0-9]{3}-?[0-9]{4}/
+    if(!ele.value.match(regex))
+    {
+        ele.style.border="2px solid red";
+        return false;
+    } else{
+        ele.style.border="1px solid black";
+        return true;
+    }
+}
+
+function validateStartDate(ele) {
+    let startDate = new Date(ele.value);
+    let minDate = new Date('1955-01-01');
+    let currentDate = new Date();
+    if (startDate < minDate || startDate > currentDate) {
+        ele.style.border = "2px solid red";
+        alert("Please input a date between 1/1/1955 and the present.");
+        return false;
+    } else {
+        ele.style.border = "1px solid black";
+        return true;
+    }
+}
+
+function validateWorkingHours(ele) {
+    let hours = parseInt(ele.value);
+    if (hours < 1 || hours > 84) {
+        ele.style.border = "2px solid red";
+        alert("Please input a number between 1 and 84 for total working hours.");
+        return false;
+    } else {
+        ele.style.border = "1px solid black";
+        return true;
+    }
+}
+
+function validateMilitaryService(ele) {
+    let years = parseInt(ele.value);
+    if (years < 1 || years > 100) {
+        ele.style.border = "2px solid red";
+        alert("Please enter a number between 1 and 100 for military years of service.");
+        return false;
+    } else {
+        ele.style.border = "1px solid black";
+        return true;
+    }
+}
